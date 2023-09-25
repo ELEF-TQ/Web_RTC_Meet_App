287d7d27ac4b44ea180ef4f1a7c723b7e4f2941a
ELEF-TQ <tariq75aitlahklift@gmail.com>
2023-09-25 14:47:09 +0100

    setup socket.io

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,15 @@
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
+const WebHost = 'http://localhost:8080';
+
+
 function App() {
+
+  useEffect(()=> {
+    const socket = io(WebHost);
+  },[]);
+
+
   return (
     <>
     
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello")
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('disconnect', () =>{
+    console.log('user disconnected');
+  })
+
+});
+
+server.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
